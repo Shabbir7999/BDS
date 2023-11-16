8f46fcdcc8f2a0698c914afdc1d0e25dab0a9a87
Ghulam Shabbir <114503606+Shabbir7999@users.noreply.github.com>
2023-11-17 00:49:00 +0500

    Created Donor Model

---
+++ BDS/Models/Donor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BDS.Models
+{
+    public class Donor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Bloodtype { get; set; }
+        public string Phno { get; set; }
+        public string Residence { get; set; }
+        public string Email { get; set; }
+    }
+}
