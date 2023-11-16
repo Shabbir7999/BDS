096544f3d7614d23c820beeb46da88add4b3d077
Muhammad Nouman Riaz <106425390+Nouman-Riaz@users.noreply.github.com>
2023-11-17 01:24:06 +0500

    Create HomeController

---
+++ BDS/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BDS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace BDS.Controllers
+{
+	public class HomeController : Controller
+	{
+
+
+		public ActionResult Index() //home page
+		{
+			return View();
+		}
+		[HttpGet]
+		public ActionResult Register()
+		{
+			return View();
+		}
+	}
+}
